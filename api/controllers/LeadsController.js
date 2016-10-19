9bddff4d0c4b1b3b1977ceb4a5337243ef96140e
Steven Zeiler <zeiler.steven@gmail.com>
2016-10-19 01:30:58 -0400

    Log leads requests

--- api/controllers/LeadsController.js
+++ api/controllers/LeadsController.js
@@ -11,9 +11,17 @@ module.exports = {
    * `LeadsController.create()`
    */
   create: function (req, res) {
+    sails.log.info("lead:create", req.body)
     Lead.create(req.body).then(lead => {
+      sails.log.info("lead:created", lead)
       return res.status(201).json({
         lead: lead
+      });
+    })
+    .catch(error => {
+      sails.log.info("lead:create:error", error.message)
+      return res.status(500).json({
+        error: error.message
       });
     })
   },
@@ -23,9 +31,16 @@ module.exports = {
    * `LeadsController.index()`
    */
   index: function (req, res) {
+    sails.log.info("lead:all")
     Lead.find().then(leads => {
       return res.status(200).json({
         leads: leads
+      });
+    })
+    .catch(error => {
+      sails.log.info("lead:all:error", error.message)
+      return res.status(500).json({
+        error: error.message
       });
     })
   }
